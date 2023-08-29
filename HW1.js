3304a7ba1fc3c203211b46998fcd0c7f23d46b93
Sevagold <113907174+Sevagold@users.noreply.github.com>
2023-08-29 16:52:49 +0300

    1dz

---
+++ HW1.js
@@ -0,0 +1,71 @@
+
+
+
+
+
+
+
+
+
+
+//  31. Если age_1 >=  age_2 и age_1 <  age_3, вывести в консоль “Welcome  !”
+//  32. Если age_1  > age_3, вывести в консоль “Keep calm and look Culture channel”.
+//  33. Иначе выводите “Technical work”.
+
+
+var item_1;
+item_1 = 5;
+console.log("item1 is " +item_1);
+var item_2;
+item_2 = 3;
+console.log("item2 is " +item_2);
+var item_3 = item_1 + item_2;
+console.log("item3 is " +item_3);
+var item_4;
+item_4 = "Yolochka";
+console.log(item_4);
+var r1 = item_3 + item_4;
+var r2 = item_3 * item_4;
+console.log(r1,r2);
+var item_5 = item_3;
+var item_6;
+var item_6_type;
+item_6 = 15;
+var item_6_type = typeof item_6;
+console.log("item_6 ==" + item_6, "item_6_type ==" + item_6_type);
+var item_7 = "item_6";
+var item_7_type = typeof item_7;
+console.log("item_7 ==" + item_7, "item_7_type ==" + item_7_type);
+var age_1 = 10;
+var age_2 = 18;
+var age_3 = 60;
+
+
+if (age_1 == 10){
+    console.log("good");
+};
+
+if (age_1 < age_2) {
+    console.log("You don't have acces cause your age is " + age_1 + " It's less then " + age_2)
+} else if (age_1 >= age_2 && age_1 < age_3) {
+    console.log("Welcome  !")
+} else if (age_1 > age_3) {
+    console.log("Keep calm and look Culture channel")
+} else console.log("Technical work");
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log()
+
+
+
